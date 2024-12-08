71677998c668ae02edc6d00a9eab6e70a603bf15
dongminkk <beagleeg12@daum.net>
2024-12-08 20:38:03 +0900

    변경

--- CloudProject/aws_management/ec2_manager/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Ec2ManagerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ec2_manager'
--- CloudProject/aws_management/ec2_manager/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('instances/', views.list_instances, name='list_instances'),  # 인스턴스 목록
-]
